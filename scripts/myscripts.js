787f85415a98ef5a967a3120bfc0b6dcef779dac
Omar <omarabukhait9622@gmail.com>
2024-08-23 17:50:27 +0300

    update

---
+++ scripts/myscripts.js
@@ -0,0 +1,12 @@
+var button = document.querySelector('button');
+var box = document.getElementById('changeIt');
+
+function changeColor(){
+    if(box.style.background == 'red'){
+        box.style.background = 'blue';
+    }else{
+        box.style.background ='red';
+
+    }
+   
+}
